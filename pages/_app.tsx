c8364193db8e567140482126a0672ec4c411017a
Steven Tey <stevensteel97@gmail.com>
2021-06-16 14:54:43 -0600

    fixed something

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { AppProps } from 'next/app'
 
-import '../styles/index.css'
+import 'tailwindcss/tailwind.css'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
