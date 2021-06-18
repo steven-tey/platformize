9cd5e02440705c3ece46869cf2f28407e4c2bb54
Steven Tey <stevensteel97@gmail.com>
2021-06-18 00:43:42 -0600

    added tailwind again

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { AppProps } from 'next/app'
-
-import 'tailwindcss/tailwind.css'
+import '../styles/global.css';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />
