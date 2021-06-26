5626d4c058658e0490e3d7a3baefb66916226076
Steven Tey <stevensteel97@gmail.com>
2021-06-26 16:40:46 -0600

    remove random string

--- pages/api/save.ts
+++ pages/api/save.ts
@@ -13,7 +13,7 @@ function makeid(length) {
 
 export default async function Save(req, res) {
     const data = JSON.parse(req.body)
-    const randomString = makeid(5)
+    //const randomString = makeid(5)
     const response = await prisma.post.update({
         where: {
             id: data.id
@@ -23,7 +23,7 @@ export default async function Save(req, res) {
             description: data.description,
             image: `https://og-image.vercel.app/${encodeURIComponent(data.title)}.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-white.svg`,
             content: data.content,
-            slug: `${data.title.replace(/[.,\/'"?'#!$%@\^&\*;:{}=\-_`~()]/g,"").replace(/\s+/g, '-').toLowerCase()}-${randomString}`
+            slug: `${data.title.replace(/[.,\/'"?'#!$%@\^&\*;:{}=\-_`~()]/g,"").replace(/\s+/g, '-').toLowerCase()}`
         }
     })
     
