8cb2cc1644eb48a99ae979db4d51f2655e24c54e
Steven Tey <stevensteel97@gmail.com>
2021-07-02 10:36:35 -0600

    added shimmer function for blog posts

--- pages/p/[slug].tsx
+++ pages/p/[slug].tsx
@@ -9,6 +9,25 @@ import Layout from '../../components/Layout'
 import prisma from '../../lib/prisma'
 import Image from 'next/image'
 
+const shimmer = (w, h) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#333" offset="20%" />
+      <stop stop-color="#222" offset="50%" />
+      <stop stop-color="#333" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#333" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`
+
+const toBase64 = (str) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
+
 export default function Post ({publicationName, postTitle, description, logo, thumbnail, content}) {
 
   return (
