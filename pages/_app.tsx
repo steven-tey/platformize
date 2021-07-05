daecd3c35f72f65ee419f0107cca009e18660a28
Steven Tey <stevensteel97@gmail.com>
2021-07-05 01:27:32 -0600

    removed loader bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,14 +7,10 @@ import LoadingBar from 'react-top-loading-bar'
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
 
-  const [progress, setProgress] = useState(0)
+  /* const [progress, setProgress] = useState(0)
 
   useEffect(() => {
     const start = () => {
-      /* console.log(Router)
-      if (Router.pathname.split('/')[1] == 'p') {
-        console.log('yay')
-      } */
       setProgress(25);
     };
     const end = () => {
@@ -28,7 +24,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       Router.events.off("routeChangeComplete", end);
       Router.events.off("routeChangeError", end);
     };
-  }, []);
+  }, []); */
 
   return (
     <>
@@ -39,14 +35,14 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         }}
         session={pageProps.session}
       >
-        <LoadingBar
+        {/* <LoadingBar
           color='#5046e4'
           height={5}
           transitionTime={100}
           waitingTime={500}
           progress={progress}
           onLoaderFinished={() => setProgress(0)}
-        />
+        /> */}
         <Component {...pageProps} />
       </Provider>
     </>
