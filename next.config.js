8dd091f0306084cdf154aa92b7a58a0384171488
Steven Tey <stevensteel97@gmail.com>
2021-07-23 18:30:44 -0600

    this should work

--- next.config.js
+++ next.config.js
@@ -14,119 +14,38 @@ module.exports = {
       ignoreBuildErrors: true,
     },
     async redirects() {
-      if (process.env.NODE_ENV === 'production') { // production mode
-        return [   
-          // redirects for app
-          {
-              source: '/publications/:path*',
-              has: [{
-                  type: 'host',
-                  value: `${process.env.APP_SLUG}.${process.env.ROOT_URL}`
-              }],
-              destination: `/${process.env.APP_SLUG}`,
-              permanent: true
-          },
-          {
-              source: `/${process.env.APP_SLUG}`,
-              has: [{
-                  type: 'host',
-                  value: `${process.env.APP_SLUG}.${process.env.ROOT_URL}`
-              }],
-              destination: `/${process.env.APP_SLUG}`,
-              permanent: true
-          },
-          {
-              source: `/${process.env.APP_SLUG}/:path*`,
-              has: [{
-                  type: 'host',
-                  value: `${process.env.APP_SLUG}.${process.env.ROOT_URL}`
-              }],
-              destination: `/${process.env.APP_SLUG}`,
-              permanent: true
-          },
-
-          // redirects for subdomains
-          {
-              source: '/publications/:path*',
-              has: [{
-                  type: 'host',
-                  value: `(?<url>.*).${process.env.ROOT_URL}`
-              }],
-              destination: '/publications/:url',
-              permanent: true
-          },
-          {
-              source: `/${process.env.APP_SLUG}`,
-              has: [{
-                  type: 'host',
-                  value: `(?<url>.*).${process.env.ROOT_URL}`
-              }],
-              destination: '/publications/:url',
-              permanent: true
-          },
-          {
-              source: `/${process.env.APP_SLUG}/:path*`,
-              has: [{
-                  type: 'host',
-                  value: `(?<url>.*).${process.env.ROOT_URL}`
-              }],
-              destination: '/publications/:url',
-              permanent: true
-          },
-
-          // redirects for custom domains
-          {
-              source: '/publications/:path*',
-              has: [{
-                  type: 'host',
-                  value: `(?<url>.*)`
-              }],
-              destination: '/publications/:url',
-              permanent: true
-          },
-          {
-              source: `/${process.env.APP_SLUG}`,
-              has: [{
-                  type: 'host',
-                  value: `(?<url>.*)`
-              }],
-              destination: '/publications/:url',
-              permanent: true
-          },
-          {
-              source: `/${process.env.APP_SLUG}/:path*`,
-              has: [{
-                  type: 'host',
-                  value: `(?<url>.*)`
-              }],
-              destination: '/publications/:url',
-              permanent: true
-          },
-        ]
-      } else { // development mode
-        return [
-          {
-              source: `/${process.env.APP_SLUG}`,
-              destination: `/`,
-              permanent: true
-          },
-          {
-              source: `/${process.env.APP_SLUG}/:path*`,
-              destination: `/`,
-              permanent: true
-          },
-          {
-              source: '/publications',
-              destination: `/`,
-              permanent: true
-          },
-          {
-              source: '/publications/:path*',
-              destination: `/`,
-              permanent: true
-          },
-        ]
-      }
+      return [
+        {
+            source: `/${process.env.APP_SLUG}`,
+            destination: `/`,
+            permanent: true
+        },
+        {
+            source: `/${process.env.APP_SLUG}/:path*`,
+            destination: `/`,
+            permanent: true
+        },
+        {
+            source: '/publications',
+            destination: `/`,
+            permanent: true
+        },
+        {
+            source: '/publications/:path*',
+            destination: `/`,
+            permanent: true
+        },
+        {
+            source: '/publication',
+            destination: `/`,
+            permanent: true
+        },
+        {
+            source: '/post',
+            destination: `/`,
+            permanent: true
+        },
+      ]
     },
     async rewrites() {
         if (process.env.NODE_ENV === 'production') { // production mode
