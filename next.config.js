b7eb6eec09d97606d6229db4745db9260bde7529
Steven Tey <stevensteel97@gmail.com>
2021-08-27 19:26:44 -0700

    take 2

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ module.exports = {
     publicRuntimeConfig: {
       ROOT_URL: process.env.ROOT_URL
     },
+    i18n: {
+        locales: ['en-US', 'fr', 'nl-NL'],
+        defaultLocale: 'en-US',
+    },
     images: {
         domains: ['og-image.vercel.app', 'pbs.twimg.com', 'avatars.githubusercontent.com']
     },
