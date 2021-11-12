9730b3e03c867b885292871dd931e7f807541b96
Steven Tey <stevensteel97@gmail.com>
2021-11-11 17:13:13 -0800

    updated middleware to latest api

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,31 +1,30 @@
-import type { EdgeRequest, EdgeResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: EdgeRequest, res: EdgeResponse, next) {
+export async function middleware(req: NextRequest) {
+    const { pathname } = req.nextUrl
     const hostname = req.headers.get('host')
 
     const currentHost = process.env.NODE_ENV === 'production' && process.env.VERCEL === '1'
                         ? hostname.replace(`.${process.env.NEXT_PUBLIC_ROOT_URL}`, '')
                         : hostname.replace(`.localhost:3000`, '')
 
-    if (req.url.pathname.startsWith(`/sites`) || req.url.pathname.startsWith(`/app`)) {
-        return res.redirect(`/`)
+    if (pathname.startsWith(`/sites`)) {
+        return new Response(null, { status: 404 })
     }
     
     if (
-        !req.url.pathname.includes('.') &&
-        !req.url.pathname.startsWith('/api')
+        !pathname.includes('.') &&
+        !pathname.startsWith('/api')
     ) {
         if (currentHost == 'app') {
-            if (req.url.pathname === '/login' && (req.cookies['next-auth.session-token'] ||  req.cookies['__Secure-next-auth.session-token'])){
-                return res.redirect('/');
+            if (pathname === '/login' && (req.cookies['next-auth.session-token'] ||  req.cookies['__Secure-next-auth.session-token'])){
+                return NextResponse.redirect('/');
             }
-            return res.rewrite(`/app${req.url.pathname}`)        
+            return NextResponse.rewrite(`/app${pathname}`)        
         } else if (currentHost == 'localhost:3000' || currentHost == process.env.NEXT_PUBLIC_ROOT_URL) {
-            return res.rewrite(`/home${req.url.pathname}`)        
+            return NextResponse.rewrite(`/home${pathname}`)        
         }
 
-        return res.rewrite(`/sites/${currentHost}${req.url.pathname}`)
+        return NextResponse.rewrite(`/sites/${currentHost}${pathname}`)
     }
-    
-    next();
 }
