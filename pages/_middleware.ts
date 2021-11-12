52a7675ccc72d6ecbdf62465aecd46b267c66d0f
Steven Tey <stevensteel97@gmail.com>
2021-11-11 17:18:27 -0800

    Update _middleware.ts

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(req: NextRequest) {
+export default function middleware(req: NextRequest) {
     const { pathname } = req.nextUrl
     const hostname = req.headers.get('host')
 
