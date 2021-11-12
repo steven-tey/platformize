c990a28d9e6286aa1ad4e7c8e085a63cff851264
Steven Tey <stevensteel97@gmail.com>
2021-11-12 11:09:31 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ module.exports = {
     images: {
         domains: ['og-image.vercel.app', 'pbs.twimg.com', 'avatars.githubusercontent.com']
     },
+    i18n: {
+      locales: ['en-US', 'fr', 'nl-NL'],
+      defaultLocale: 'en-US',
+    },
     typescript: {
       // !! WARN !!
       // Dangerously allow production builds to successfully complete even if
