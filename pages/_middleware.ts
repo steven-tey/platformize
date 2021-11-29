1a011ef17f3b68167bc92cc1bb993c1a7155b938
Steven Tey <stevensteel97@gmail.com>
2021-11-29 13:56:24 -0500

    added changes for preview deployments

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -5,13 +5,15 @@ export default function middleware(req: NextRequest) {
     const hostname = req.headers.get('host') // retreat.staging.platformize.co
 
     // regex for vercel.app wildcards: (\.)?([\w-]+).vercel.app
-    const regex = /((\.)?platformize.co|(\.)?staging.platformize.co)/
+    const regex = /((\.)?([\w-]+).vercel.app|(\.)?platformize.co|(\.)?staging.platformize.co)/
 
     const currentHost = process.env.VERCEL === '1' ? hostname.replace(regex, '') : hostname.replace(`.localhost:3000`, '')
 
     if (pathname.startsWith(`/sites`) && !hostname.endsWith('.vercel.app')) { // prevent canonical link access on subdomains & custom domains
         return new Response(null, { status: 404 })
     }
+    
+    const previewDeployment = hostname.endsWith('.vercel.app')
     
     if (
         !pathname.includes('.') &&
@@ -21,13 +23,13 @@ export default function middleware(req: NextRequest) {
             if (pathname === '/login' && (req.cookies['next-auth.session-token'] ||  req.cookies['__Secure-next-auth.session-token'])){
                 return NextResponse.redirect('/');
             }
-            return NextResponse.rewrite(`/app${pathname}`)        
+            return NextResponse.rewrite(previewDeployment ? pathname : `/app${pathname}`)        
         } else if (
             currentHost === 'localhost:3000' || currentHost === ''
         ) {
-            return NextResponse.rewrite(`/home${pathname}`)        
+            return NextResponse.rewrite(previewDeployment ? pathname : `/home${pathname}`)        
         }
 
-        return NextResponse.rewrite(`/sites/${currentHost}${pathname}`)
+        return NextResponse.rewrite(previewDeployment ? pathname : `/sites/${currentHost}${pathname}`)
     }
 }
